a6738cf16623cb78488703f2c859a72dc15262b7
naraynabhay <abhaynarayan07062002@gmail.com>
2024-05-24 22:08:58 +0530

    commit

---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,86 @@
+/**
+ * Portfolio component
+ *
+ * Highlights some of  your creations. These can be designs, websites,
+ * open source contributions, articles you've written and more.
+ *
+ * This is a great area for you to to continually add to and refine
+ * as you continue to learn and create.
+ */
+
+import React from "react";
+
+/**
+ * Desk image
+ *
+ * Below is a sample desk image. Feel free to update this to an image of your choice,
+ * updating below imageAltText to string that represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a photo you
+ * freely use on your site.
+ */
+import image from "../images/design-desk.jpeg";
+
+const imageAltText = "desktop with books and laptop";
+
+/**
+ * Project list
+ *
+ * An array of objects that will be used to display for your project
+ * links section. Below is a sample, update to reflect links you'd like to highlight.
+ */
+const projectList = [
+  {
+    title: "E-Commerce website",
+    description:
+      " enables buyers to purchase high-value products directly, fostering a sustainable and diverse agricultural ecosystem.",
+    url: "https://mernstack-2024.vercel.app/",
+  },
+  {
+    title: "ECE Society Website",
+    description:
+      "with HTML, CSS, JavaScript, and backend using Node.js. Successfully launched, enhancing online presence and community communication",
+    url: "https://ecesociety.bitsindri.ac.in/",
+  },
+  {
+    title: "REAL TIME VEHICLE TRACKING SYSTEM",
+    description:
+      "Revolutionized employee transportation with a MERN-based real-time for the Hackthoan organised by Vedanta    ESL which includes tracking system. Streamlined operations, enabled online bookings, and enhanced efficiency through strategic broker collaboration, minimizing manual work.",
+    url: "https://hackhathonbsl.netlify.app/",
+  },
+  // {
+  //   title: "GitHub Codespaces and github.dev",
+  //   description:
+  //     "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
+  //   url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+  // },
+];
+
+const Portfolio = () => {
+  return (
+    <section className="padding" id="portfolio">
+      <h2 style={{ textAlign: "center" }}>Project</h2>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src={image}
+            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            alt={imageAltText}
+          />
+        </div>
+        <div className="container">
+          {projectList.map((project) => (
+            <div className="box" key={project.title}>
+              <a href={project.url} target="_blank" rel="noopener noreferrer">
+                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+              </a>
+              <p className="small">{project.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
